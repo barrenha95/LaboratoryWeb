0d8a97c9fb38d03f1f439b477607cfea679a3ce2
joao <barrenha95@yahoo.com.br>
2024-05-02 06:55:02 -0300

    user-class

--- LabWeb/src/main/java/entities/User.java
+++ LabWeb/src/main/java/entities/User.java
@@ -1,27 +1,100 @@
 package entities;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Objects;
+import java.io.Serializable;
 
-@SpringBootApplication
-public class User {
-
-	private final int id;
-	private final String nome;
-	private final String email;
-	private final int telefone;
-	private final String password;
-
-	public User(final int id,
-				final String nome,
-				final String email,
-				final int telefone,
-				final String password) {
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	// Basic attributes
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String password;
+	
+	// Empty constructor
+	public User() {
+		
+	}
+	
+	// Constructor using all the attributes
+	public User(Long id, String nome, String email, String telefone, String password) {
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.telefone = telefone;
 		this.password = password;
 	}
+	
+	// Getters and setters
+	// Id
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	// nome
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	// email
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	// telefone
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	//password
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, nome, password, telefone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
+				&& Objects.equals(password, other.password) && Objects.equals(telefone, other.telefone);
+	}
+	
+	
+	
+	
 }
